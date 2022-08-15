f780e454ec570627e521afebe1d473fb96547b0c
marcelomaiaf <marcelo.filho@sou.inteli.edu.br>
2022-08-15 14:19:12 -0300

    atividade semana 3: listas e fucoes

---
+++ SEMANA_03/02_ATIVIDADES/atividade de programação LISTAS E FUNÇÕES/Node2D.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+
+var array1 = ["one", "two", "three", "four", "five"] #atividade 1
+
+
+func _on_ButtonAtividade1_pressed():
+	$Label/Label.text = str(array1)
+
+
+var array2 = [] #atividade 2
+
+
+func _on_ButtonAtividade2_pressed():
+	array2.append($Label2/TextEdit.text)
+	$Label2/Label.text = str(array2)
+
+func _on_ButtonAtividade3_pressed():
+	$Label3/Label.text = "olá, mundo"
+
+
+
+func _on_ButtonAtividade4_pressed():
+	$Label4/Label.text = $Label4/TextEdit.text
