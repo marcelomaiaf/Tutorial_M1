b978d2225d445f6014d368e6dc76b77ae87d9f68
marcelomaiaf <marcelo.filho@sou.inteli.edu.br>
2022-09-30 20:40:04 -0300

    algoritmos versao final atividade ponderada

--- SEMANA_09/02_ATIVIDADES/algoritmo/Node2D.gd
+++ SEMANA_09/02_ATIVIDADES/algoritmo/Node2D.gd
@@ -8,7 +8,6 @@ var lineEdit
 func _on_button_pressed():
 	lineEdit = float($LineEdit.text)
 	array.append(lineEdit)
-	print(array)
 	if len(array) >= 10:
 		$button.queue_free()
 		
@@ -22,4 +21,4 @@ func _on_ordenar_button_up():
 				var new = array[x]
 				array[x] = array[x+1]
 				array[x+1] = new
-	$ColorRect/Label3.text = str(array)
+	$ColorRect/num.text = str(array)
